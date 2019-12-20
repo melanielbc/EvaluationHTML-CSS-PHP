f558d335a158c4957b6529705cbb96136aac05a6
Mélanie Leblanc <themelanieleblanc99@gmail.com>
2019-12-20 11:53:19 +0100

    formulaire

--- includes/frmContact.php
+++ includes/frmContact.php
@@ -1,31 +1,34 @@
-<form action="index.php?page=contact" method="post">
-  <fieldset>
-    <legend>Entre tes données ici</legend>
-  <div>
-    <label for="nom">Nom :</label>
-    <input type="text" name="nom" id="nom" value="<?=$nom ?>"/>
-  </div>
-  <div>
-    <label for="prenom">Prénom :</label>
-    <input type="text" name="prenom" id="prenom" value="<?=$prenom ?>"/>
-  </div>
-  <div>
-    <label for="telephone">Téléphone :</label>
-    <input type="text" name="telephone" id="telephone" value="<?=$telephone ?>"/>
-  </div>
-  <div>
-    <label for="mail">e-mail :</label>
-    <input type="email" name="mail" id="mail" value="<?=$mail ?>"/>
-  </div>
-  </fieldset>
-  <fieldset>
-  <div>
-    <label for="msg">Message :</label>
-    <textarea name="msg" id="msg"><?=$msg ?></textarea>
-  </div>
-  <div>
-    <input type="submit" value="Clique ici pour me le transmettre" />
-  </div>
-  </fieldset>
-  <input type="hidden" name="frmContact" />
-</form>
+<div class="wrap">
+  <form action="index.php?page=contact" method="post">
+    <fieldset>
+      <legend>Entre tes données ici</legend>
+    <div>
+      <label for="nom">Nom :</label>
+      <input type="text" name="nom" id="nom" value="<?=$nom ?>"/>
+    </div>
+    <div>
+      <label for="prenom">Prénom :</label>
+      <input type="text" name="prenom" id="prenom" value="<?=$prenom ?>"/>
+    </div>
+    <div>
+      <label for="telephone">Téléphone :</label>
+      <input type="text" name="telephone" id="telephone" value="<?=$telephone ?>"/>
+    </div>
+    <div>
+      <label for="mail">e-mail :</label>
+      <input type="email" name="mail" id="mail" value="<?=$mail ?>"/>
+    </div>
+    </fieldset>
+    <fieldset>
+    <div>
+      <label for="msg">Message :</label>
+      <textarea name="msg" id="msg"><?=$msg ?></textarea>
+    </div>
+    <div>
+      <input type="submit" value="Clique ici pour me le transmettre" />
+    </div>
+    </fieldset>
+    <input type="hidden" name="frmContact" />
+  </form>
+</div>
+<div class="clear"></div>
