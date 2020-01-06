020c0dd15706fa0adaaf81b3d0f63a6a65d9d7bd
Mélanie Leblanc <themelanieleblanc99@gmail.com>
2020-01-06 11:06:56 +0100

    cv commit

--- includes/cvEtranger.inc.php
+++ includes/cvEtranger.inc.php
@@ -1,1 +1,46 @@
-<h2>CV en langue étrangère</h2>
+<section id="cv-anglais">
+  <div class="wrap">
+
+
+
+    <h3>Mélanie Leblanc</h3>
+    <h3>Référent digital</h3>
+
+    <p>05/10/1999 <br />00 street of hope<br /> 76190 YVETOT <br /><a href="mail:mmmmmmmm@mmm.com">mmmmmmmm@mmm.com</a> <br />
+      <a href="tel:00 00 00 00 00">00 00 00 00 00</a> <br />Driver's licence (B)</p>
+
+      <h4>Diplomas and training</h4>
+      <p>Formation Référent Digital, Campus Saint Marc of Rouen (2019-2020) <br /><br />BTS Management business units, high school Flaubert of Rouen (2019) <br /><br />2017 Baccalaureat STMG option Mercatique (mention well enought), high schoolRaymond Queneau of Yvetot (2017) <br /><br />Certificate of general education (mention well enought), college Bobée of Yvetot (2014)</p>
+
+      <h4>Experiences professionals</h4>
+      <h5>Saleswomen in CDD and 13 weeks internship at Princesse Tam Tam, lingerie shop</h5>
+      <p>July, December 2018 and January-February, April-September, November 2019<br /><br />Sale, reception and customer advice, window, merchandising, collections, package processing, labelling, reassortation, opening and closing of the store.</p>
+      <h5>Care taking of cat</h5>
+      <p>July-August, December 2018 and July-August 2019<br /><br />Feeding, bedding change and company feed.</p>
+      <h5>Job-shadowing</h5>
+      <p>Cache Cache, ready-to-wear shop Yvetot 2014<br /><br />Shelf placement, installation of anti-theft badge on clothing and customer reception.</p>
+
+      <h4>Skills</h4>
+      <p>Languages :</p>
+      <ul class="puces">
+        <li>English LV1 : grade level</li>
+        <li>Spanish LV2 : grade level</li>
+      </ul>
+      <p>Program supports</p>
+      <ul class="puces">
+        <li>The Microsoft Office Suite</li>
+        <li>Adobe : Photoshop, Illustrator, Première Pro and Indesign</li>
+        <li>Atome</li>
+        <li>Gantt project</li>
+      </ul>
+
+      <h4>Hobbies</h4>
+      <ul class="puces">
+        <li>Sports : Dance for 11 years, Gym with a competition.</li>
+        <li>Music : Practiced flute for 13 years, 3rd cycle in solfege and having been part of an armonic orchestra for 7 years.</li>
+        <li>Exchange trip: England, Spain and Germany.</li>
+      </ul>
+
+  </div>
+
+</section>
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,6 @@
     <ul>
       <li><a href="index.php?page=accueil" title="Page d'accueil">Accueil</a></li>
       <li><a href="index.php?page=cv" title="Voici mon CV">CV</a></li>
-      <li><a href="index.php?page=cvEtranger" title="Voici mon CV dans une autre langue">CV autre langue</a></li>
       <li><a href="index.php?page=contact" title="Contacter nous">Contact</a></li>
       <li><a href="index.php?page=mentionsLegales" title="Les mentions légales">Mentions légales</a></li>
     </ul>
