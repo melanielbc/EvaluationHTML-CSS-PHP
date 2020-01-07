7b15174fcda5dc5412fa5ac138568bca04c24120
Mélanie Leblanc <themelanieleblanc99@gmail.com>
2020-01-07 13:41:56 +0100

    image

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -3,8 +3,10 @@
   <div class="wrap">
     <h3>Présentation</h3>
     <p>Voici mon site intitulé Le site de Mélanie Leblanc, je m'appelle Mélanie Leblanc (très étonnant!) et je vais vous expliquer ma formation.<br /><br />Mais avant tout, qui suis-je?<br />Je suis une fille, née à <a href="https://www.rouen.fr/">Rouen</a>, J'ai ensuite habitée dès mon plus jeune âge à <a href="http://www.yvetot.fr/">Yvetot</a>. Je suis née jeune et je fête mon anniversaire tous les ans.<br /><br />J'ai vécu au milieux de la nature et des animaux (moutons, poules, lapins, cochon d'inde, chats, poissons d'intérieur et d'extérieur), j'ai même un jour jouée la vétérinaire en prenant sous mon aile un pigeon qui avait une aile cassée et qui allait se faire manger par mon chat. J'ai donc pris le pigeon dans mes mains et je l'ai mis dans une cage que j'avais chez moi, j'ai donc pris soin de ce pigeon pendant deux mois. Je lui ai donnée à manger et de l'eau, donc deux mois plus tard j'ai relachée ce pigeon qui a pris son envole. Pourquoi deux mois? Parce que l'aile du pigeon n'était pas reconstruite.
+      <img id="millions" src="./assets/image/30millionsdamis.png" alt="les animaux c'est la vie">
     <br /><br /> J'ai également eu mon permis B, c'est celui en voiture avec l'auto-école Jacques. Je l'ai eu au bout de deux fois, pourquoi pas la première fois? parce que le l'ai ratée à cause d'un contresens sur un parking d'Auzebosc, la sorti etais juste à ma gauche mais il fallait que je refasse tout le tour donc FAUTE ELIMINATOIRE !!! Je l'ai donc repassée une semaine après et je l'ai eu, j'ai repassée sur ce fameux parking mais je n'est pas refait cette faute bien sur !
     </p>
+
 
     <h3>Formation</h3>
     <p>Je suis actuellement en formation Référent Digital chez la <a href="https://nfactory.school/">NFactory School</a> au <a href="https://campus-saint-marc.com/">Campus Saint Marc</a>.</p>
